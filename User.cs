8e82bbb4b44d6d2945fdff0859c765e716f5b201
Greeshmm <35794450+Greeshmm@users.noreply.github.com>
2019-01-31 08:51:37 +0530

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Database
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name="User Name")]
+        public String name { get; set; }
+        [Required]
+        public String Uname { get; set; }
+        [Required]
+        [MaxLength(8,ErrorMessage="maximum length 8 to 10")]
+        [DataType(DataType.Password)]
+        public String password { get; set; }
+    }
+}
---
+++ UserContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Database.Models
+{
+    public class UserContext:DbContext
+    {
+        public UserContext()
+            : base("mycon")
+        {
+            DropCreateDatabaseIfModelChanges<User
+        }
+    }
+}
